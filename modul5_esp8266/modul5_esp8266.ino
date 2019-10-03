c45148888b506db583d2c9a2c5387c7338828353
rasisbuldan <rasisbuldan@gmail.com>
2019-10-03 07:56:29 +0700

    Updated Arduino MQTT

--- modul5_esp8266/modul5_esp8266.ino
+++ modul5_esp8266/modul5_esp8266.ino
@@ -39,8 +39,6 @@ char msg[50];
 int value = 0;
 
 void setup_wifi() {
-
-  delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -48,6 +46,7 @@ void setup_wifi() {
 
   WiFi.begin(ssid, password);
 
+  // Waiting until connected
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -61,41 +60,35 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+  Serial.print("Message received on topic ");
   Serial.print(topic);
-  Serial.print("] ");
+  Serial.print(": ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
+    
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
+    
     // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str())) { //Send first confirmation message if connected
       Serial.println("connected");
-      client.publish("topic/sensor1", "123");
+      client.publish("topic/sensor1", "0");
       client.subscribe("topic/sensor2");
+      client.subscribe("topic/ledstatus1");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
+      
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -103,7 +96,6 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
